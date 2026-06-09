b67dd40f8f2ff85d513187847bbe9347fd17fe3d
Owllight <precious.owllight@gmail.com>
2026-06-09 20:46:52 +0700

    Adding more autopairs for html style tags.

--- SETUP.md
+++ SETUP.md
@@ -1,4 +1,5 @@
 What to install for everyday usage :
+
 - Ripgrep
 - fd
 - tree-sitter-cli (from cargo)
@@ -8,7 +9,7 @@ Additional Download for minimizing warnings and errors in :checkhealth
 In Fedora :
 sudo dnf upgrade --refresh
 
-sudo dnf install magick go luarocks ruby gem java javac julia php composer python3-pip python3-wheel gs pdflatex sqlite3
+sudo dnf install magick go luarocks ruby gem java javac julia php composer python3-pip python3-wheel gs pdflatex sqlite3 rustfmt
 
 sudo dnf copr enable dejan/lazygit
 
--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,19 +1,44 @@
 vim.pack.add({
-    "https://github.com/windwp/nvim-autopairs",
+	"https://github.com/windwp/nvim-autopairs",
+	"https://github.com/windwp/nvim-ts-autotag",
 })
 
 local group = vim.api.nvim_create_augroup("NvimAutopairsLazyLoad", { clear = true })
 
 vim.api.nvim_create_autocmd("InsertEnter", {
-    pattern = "*",
-    group = group,
-    once = true,
-    callback = function()
-        require("nvim-autopairs").setup({
-            disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input", "vim" },
-            disable_in_macro = true,
-            ignore_next_char = [=[[%w%%%'%[%"%.%'%$]]=],
-            enable_moveright = true,
-        })
-    end,
+	pattern = "*",
+	group = group,
+	once = true,
+	callback = function()
+		require("nvim-autopairs").setup({
+			disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input", "vim" },
+			disable_in_macro = true,
+			ignore_next_char = [=[[%w%%%'%[%"%.%'%$]]=],
+			enable_moveright = true,
+		})
+	end,
+})
+
+require("nvim-ts-autotag").setup({
+	opts = {
+		-- Defaults
+		enable_close = true, -- Auto close tags
+		enable_rename = true, -- Auto rename pairs of tags
+		enable_close_on_slash = true, -- Auto close on trailing </
+	},
+	-- Also override individual filetype configs, these take priority.
+	-- Empty by default, useful if one of the "opts" global settings
+	-- doesn't work well in a specific filetype
+	per_filetype = {
+		["html"] = {
+			enable_close = true,
+			enable_rename = true,
+			enable_close_on_slash = true,
+		},
+		["php"] = {
+			enable_close = true,
+			enable_rename = true,
+			enable_close_on_slash = true,
+		},
+	},
 })
