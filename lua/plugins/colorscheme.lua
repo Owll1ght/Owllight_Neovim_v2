55f331f187cff991b5864629c289f87b2cc043e2
Jason Owllight <jason.s2412@gmail.com>
2026-06-22 06:22:02 +0700

    Adding configuration for changing persistent colorscheme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,7 +7,39 @@ require("kanagawa").setup({
 	},
 })
 
-vim.cmd("colorscheme kanagawa")
+local theme_file = vim.fn.stdpath("data") .. "/current_theme.txt"
+
+local function load_saved_theme()
+	local file = io.open(theme_file, "r")
+	if file then
+		local saved_theme = file:read("*l")
+		file:close()
+
+		if saved_theme and saved_theme ~= "" then
+			local ok = pcall(function()
+				vim.cmd("colorscheme " .. saved_theme)
+			end)
+
+			if ok then
+				return
+			end
+		end
+	end
+	vim.cmd("colorscheme kanagawa-wave")
+end
+
+load_saved_theme()
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function(args)
+		local file = io.open(theme_file, "w")
+		if file then
+			file:write(args.match)
+			file:close()
+		end
+	end,
+})
 
 local map = vim.keymap.set
 
